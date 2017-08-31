9610a4d5700a5ba3605f761922afd0dc111c426c
unknown <orhan.hajderovic@hotmail.com>
2017-08-31 20:09:48 +0200

    Add necessary auth components + user model file

---
+++ client/src/app/auth/user.model.ts
@@ -0,0 +1,11 @@
+export class User{
+    userName: String;
+    password: String;
+    email: String;
+    firstName: String;
+    lastName: String;
+    phone: String;
+    city: String;
+    postalCode: String;
+    adress: String;
+}
